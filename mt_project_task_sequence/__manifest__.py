ce785fd6994a563ff24fddf05710690994227e5a
Htet Aung Shane <mmc.htetaungshane@gmail.com>
2026-03-13 11:23:12 +0700

    Update For Task Kanban

--- mt_project_task_sequence/__manifest__.py
+++ mt_project_task_sequence/__manifest__.py
@@ -4,7 +4,7 @@
     "application": True,
     "installable": True,
     "license": "LGPL-3",
-    "version": "17.0.0.1",
+    "version": "18.0.0.1",
     "depends": ["project"],
     "data": [
         "views/project_ext.xml",
